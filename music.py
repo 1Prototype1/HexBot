7114cf2dcdc0675393ba93e050cae21958276162
1Prototype1 <astonlopes1999@gmail.com>
2020-08-26 22:39:18 +0530

    QuickPoll added!

--- music.py
+++ music.py
@@ -192,6 +192,67 @@ class Music(commands.Cog):
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
+class QuickPoll(commands.Cog):
+    """QuickPoll"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='quickpoll')
+    async def quickpoll(self, ctx, question, *options: str):
+        """Create a quick poll[quickpoll "question" options]"""
+        if len(options) <= 1:
+            await ctx.send('You need more than one option to make a poll!')
+            return
+        if len(options) > 10:
+            await ctx.send('You cannot make a poll for more than 10 things!')
+            return
+
+        if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+            reactions = ['✅', '❌']
+        else:
+            reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+
+        description = []
+        for x, option in enumerate(options):
+            description += '\n {} {}'.format(reactions[x], option)
+        embed = discord.Embed(title=question, description=''.join(description), color=5898184)
+        react_message = await ctx.send(embed=embed)
+        for reaction in reactions[:len(options)]:
+            await react_message.add_reaction(reaction)
+        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+        await react_message.edit(embed=embed)
+
+    @commands.command(name='tally')
+    async def tally(self, ctx, id):
+        """Tally the created poll"""
+        poll_message = await ctx.message.channel.fetch_message(id)
+        if not poll_message.embeds:
+            return
+        embed = poll_message.embeds[0]
+        if poll_message.author != bot.user:
+            return
+        if not embed.footer.text.startswith('Poll ID:'):
+            return
+        unformatted_options = [x.strip() for x in embed.description.split('\n')]
+        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
+            else {x[:1]: x[2:] for x in unformatted_options}
+        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
+        voters = [bot.user.id]  # add the bot's ID to the list of voters to exclude it's votes
+
+        tally = {x: 0 for x in opt_dict.keys()}
+        for reaction in poll_message.reactions:
+            if reaction.emoji in opt_dict.keys():
+                reactors = await reaction.users().flatten()
+                for reactor in reactors:
+                    if reactor.id not in voters:
+                        tally[reaction.emoji] += 1
+                        voters.append(reactor.id)
+
+        output = 'Results of the poll for "{}":\n'.format(embed.title) + \
+                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
+        await ctx.send(output)
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("~"),
                    description='Relatively simple music bot.')
 
@@ -202,4 +263,5 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name="Nothing"))
 
 bot.add_cog(Music(bot))
+bot.add_cog(QuickPoll(bot))
 bot.run(os.environ['BOT_Token'])
