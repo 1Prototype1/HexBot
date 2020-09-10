9046e088b11ce6e363a6804d912068f81e0f8144
1Prototype1 <astonlopes1999@gmail.com>
2020-09-10 12:20:03 +0530

    Update wallpaper

--- cogs/media.py
+++ cogs/media.py
@@ -11,20 +11,22 @@ class Media(commands.Cog):
 		self.header = {'Authorization': os.environ['Unsplash_Token']}
 		self.http = urllib3.PoolManager()
 
-	@commands.command(name='wallpaper', aliases=['wall'])
+	@commands.command(name='wallpaper', aliases=['walla'])
 	async def _wallpaper(self, ctx, *query: str):
 		"""Get wallpaper from Unsplash"""
-		fields = {}
+		fields = {'count': 1}
 		if query:
-			fields = {'query': query}
-		r = self.http.request('GET', 'https://api.unsplash.com/photos/random', headers=self.header, fields=fields)
-		r = json.loads(r.data.decode('utf-8'))
-		if not r.get('errors', None):
-			em = discord.Embed()
-			em.set_image(url=r['urls']['raw'])
-			em.set_footer(text=f"©{r['user']['name']} from Unsplash")
-			await ctx.send(embed=em)
-		else:
+			fields['query'] = query
+		results = self.http.request('GET', 'https://api.unsplash.com/photos/random', headers=self.header, fields=fields)
+		results = json.loads(results.data.decode('utf-8'))
+		try:
+			for r in results:
+				em = discord.Embed(colour=discord.Colour(0xFF355E))
+				em.set_image(url=r['urls']['raw'])
+				em.set_footer(text=f"{r['user']['name']} on Unsplash", icon_url='https://i.ibb.co/f4Xbgkv/lens.png')
+				await ctx.send(embed=em)
+		except Exception as e:
+			print(e)
 			await ctx.send('Error getting wallpaper :disappointed_relieved:')
 
 def setup(bot):
